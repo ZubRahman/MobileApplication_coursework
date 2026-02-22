cb70e0efa4a154cf3accda01eea3ab7002751752
rahmanm60 <rahmanm60@coventry.ac.uk>
2026-02-22 04:26:19 +0000

    feat: display saved diary entries in Diary tab

--- ViewModels/DiaryViewModel.cs
+++ ViewModels/DiaryViewModel.cs
@@ -1,9 +1,37 @@
+using System.Collections.ObjectModel;
+using MyMauiApp.Data.Repositories;
+using MyMauiApp.Models;
+
 namespace MyMauiApp.ViewModels;
 
 public class DiaryViewModel : BaseViewModel
 {
-    public DiaryViewModel()
+    private readonly IDiaryRepository _repo;
+
+    public ObservableCollection<DiaryEntry> Entries { get; } = new();
+
+    public DiaryViewModel(IDiaryRepository repo)
     {
-        Title = "Discover";
+        _repo = repo;
+        _ = LoadEntries();
+    }
+
+    public async Task LoadEntries()
+    {
+        try
+        {
+            var items = await _repo.GetEntriesAsync();
+
+            MainThread.BeginInvokeOnMainThread(() =>
+            {
+                Entries.Clear();
+                foreach (var e in items)
+                    Entries.Add(e);
+            });
+        }
+        catch (Exception ex)
+        {
+            System.Diagnostics.Debug.WriteLine(ex.ToString());
+        }
     }
 }
--- Views/Pages/DiaryPage.xaml.cs
+++ Views/Pages/DiaryPage.xaml.cs
@@ -1,9 +1,20 @@
+using MyMauiApp.ViewModels;
+
 namespace MyMauiApp.Views.Pages;
 
 public partial class DiaryPage : ContentPage
 {
-    public DiaryPage()
+    public DiaryPage(DiaryViewModel vm)
     {
         InitializeComponent();
+        BindingContext = vm;
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+
+        if (BindingContext is DiaryViewModel vm)
+            await vm.LoadEntries();
     }
 }
